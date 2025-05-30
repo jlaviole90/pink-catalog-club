b60ac9366a98899233de65624024f9c36577bea2
jlaviole90 <jlaviole90@gmail.com>
2025-05-29 19:23:07 -0500

    final edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { PinkHorseComponent } from './pink-horse/pink-horse.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [PinkHorseComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'pink-catalog-club';
---
+++ src/app/pink-horse/pink-horse.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pink-horse',
+  imports: [],
+  templateUrl: './pink-horse.component.html',
+  styleUrl: './pink-horse.component.scss'
+})
+export class PinkHorseComponent {
+
+}
